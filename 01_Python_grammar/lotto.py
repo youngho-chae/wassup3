0914e759e5d5b114cbfd6f9d6fbd996b8c6be856
cyh <codudgh0810@gmail.com>
2024-06-18 22:49:44 +0900

    0618

--- 01_Python_grammar/lotto.py
+++ 01_Python_grammar/lotto.py
@@ -1,9 +1,17 @@
 import random
-
+print('======Lotto 1등 가즈앙!!======')
 def 로또_번호생성():
     return random.sample(range(1, 46), 6)
 
 for i in range(5):
     번호생성 = 로또_번호생성()
     print(f"Set {i + 1}: {번호생성}")
-print('~.~ 1등 당첨 가즈앙!! ~.~')
+print('~'*30)
+print('======연금 1등 가즈앙!!======')
+import random
+def 연금_번호생성():
+    return random.sample(range(0, 10), 6)
+연금번호 = 연금_번호생성()
+print(f"각조 : {연금번호}")
+print('='*30)
+print('~.~ 1등 당첨 기원 ~.~')
